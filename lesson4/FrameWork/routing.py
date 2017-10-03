70dc7877f64625922f3d0fa325a8e1fe7d07aa5b
Alexey Radchenko <novame@mail.ru>
2017-10-03 18:42:24 +0300

    add readme

--- lesson4/FrameWork/routing.py
+++
@@ -1,7 +0,0 @@
-def route(route='/'):
-    def decorator(function, env, start_response):
-        def wrapped():
-            start_response('200 OK', [('Content-Type', 'text/html')])
-            return function
-        return wrapped
-    return decorator
