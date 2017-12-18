39cc49d2636c9d233f7ca8380721a6520facac22
AlexeyRadchenko <novame@mail.ru>
2017-12-18 22:24:54 +0500

    del comments

--- lesson24/quadratic_equation.py
+++ lesson24/quadratic_equation.py
@@ -1,7 +1,3 @@
-'''print("Введите коэффициенты для квадратного уравнения (ax^2 + bx + c = 0):")
-a = float(input("a = "))
-b = float(input("b = "))
-c = float(input("c = "))'''
 import math
 
 
@@ -15,8 +11,3 @@ def qe_roots(a, b, c):
         return round((-b + math.sqrt(d)) / (2 * a), 2), round((-b - math.sqrt(d)) / (2 * a), 2)
     elif d == 0:
         return round(-b / (2 * a), 2)
-
-'''
-print(discriminant(2, 4, 2))
-print(discriminant(3.2, -7.8, 1))
-print(discriminant(8, 4, 2))'''
